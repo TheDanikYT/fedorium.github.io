f3290f1d8daa3560828ad3985be3952e9c8c2706
TheDanik <kraschkids27@ya.ru>
2025-07-14 21:02:15 +0500

    Add files via upload

---
+++ dantrecker/add-program.php
@@ -0,0 +1,10 @@
+﻿<?php
+$targetDir = "uploads/";
+$targetFile = $targetDir . basename($_FILES["file"]["name"]);
+
+if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+    echo "Файл ". htmlspecialchars(basename($_FILES["file"]["name"])) . " успешно загружен.";
+} else {
+    echo "Ошибка загрузки файла.";
+}
+?>
